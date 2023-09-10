49c67a5c594add920049d5189227745d7f0cda98
syunsuke-I <odol@ymail.ne.jp>
2023-09-10 21:55:03 +0900

    -aの再現

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	showAll := flag.Bool("a", false, "all files")
+	flag.Parse()
 	items, err := os.ReadDir("./")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, item := range items {
-		// ファイルの情報を取得
-		file_info, err := item.Info()
-		if err != nil {
-			log.Fatal(err)
-		}
-
 		file_name := item.Name()
-		file_size := file_info.Size()
-		file_name_init := file_name[0:1]
 		// 隠しファイルは表示しない
-		if file_name_init != "." {
-			fmt.Printf("%s\t%d\n", file_name, file_size)
+		if strings.HasPrefix(file_name, ".") {
+			if *showAll {
+				println(file_name)
+				continue
+			}
+			continue
 		}
+		fmt.Println(file_name)
 	}
-
 }
