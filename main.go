ac307a89a3b7ae3a62183e5d72a6b17b5676cb72
syunsuke-I <odol@ymail.ne.jp>
2023-09-10 23:00:42 +0900

    一応完成

--- main.go
+++ main.go
@@ -10,22 +10,34 @@ import (
 
 func main() {
 	showAll := flag.Bool("a", false, "all files")
+	showDetail := flag.Bool("l", false, "show detail info")
 	flag.Parse()
 	items, err := os.ReadDir("./")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	for _, item := range items {
 		file_name := item.Name()
-		// 隠しファイルは表示しない
-		if strings.HasPrefix(file_name, ".") {
-			if *showAll {
-				println(file_name)
-				continue
+		is_hidden_file := strings.HasPrefix(file_name, ".")
+		switch {
+		case *showDetail:
+			file_info, err := item.Info()
+			if err != nil {
+				log.Fatal(err)
 			}
-			continue
+			file_size := file_info.Size()
+			mod_time := file_info.ModTime()
+			if !is_hidden_file {
+				// fmt.Printf("%10s\t%d\t%s\n", file_name, file_size, mod_time)
+				fmt.Printf("%s\t%10d%10s\n", mod_time, file_size, file_name)
+			}
+			// 隠しファイルは表示しない
+		case *showAll:
+			println(file_name)
+		default:
+			if !is_hidden_file {
+				println(file_name)
+			}
 		}
-		fmt.Println(file_name)
 	}
 }
